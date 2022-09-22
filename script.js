b41ce0a0ba550784037b54ca614b5a11dbe059b7
Oluwatomilola <oluwatomilola22@gmail.com>
2022-09-22 02:24:13 -1200

    finished javascript assignment

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+document.getElementById("title").innerHTML="THIRD DAY JAVASCRIPT.";
+document.getElementById("experience").innerHTML="I had an amazing day today being the third day,another topic was introduced as OPERATORS(an operator is a special symbol used to perform operations(values and variables). Operators has different types, namely; Arithemetic Operators, Assignment Operators, Comparision Operators, Logical Operators, Conditional Operators, Type Operators and Bitwise Operators. I will be explaining one of these in today's write-up,which is ASSIGNMENT OPERATORS."
+document.getElementById("topic").innerHTML="Assignment Operator Experience.";
+document.getElementById("topic-highlight").innerHTML="Assignment Operator assigns a value to Javascript variable. They are; Subtraction Assignment( -=), Division Assignment(/=), Multiplication Assignment(*=), Modulus Assignment(%=), Exponential Assignment(**=). I will give illustration of each below.";
+
+
+document.getElementById("sub-topic-minus").innerHTML="1. SUBTRACTION ASSIGNMENT: Subtraction assignment operator subtracts a value from a variable.";
+let x = 50;
+x -= 10;
+document.getElementById("minus-illustration").innerHTML="<code> let x = 50; </code><br> <code> x -= 10;</code>";
+document.getElementById("minus-ans").innerHTML= x;
+
+
+document.getElementById("sub-topic-division").innerHTML="2. DIVISION ASSIGNMENT: The division assignment divides a variable.";
+let y = 23;
+y /= 11;
+document.getElementById("division-illustration").innerHTML="<code> let y = 23; </code><br> <code> y /= 10;</code>";
+document.getElementById("division-ans").innerHTML= y;
+ 
+
+document.getElementById("sub-topic-multiple").innerHTML="3. MULTIPLICATION ASSIGNMENT: This assignment operator multiplies a variable.";
+let oranges = 100;
+oranges *= 11;
+document.getElementById("multiple-illustration").innerHTML="<code> let oranges = 50; </code><br> <code> oranges *= 10;</code>";
+document.getElementById("multiple-ans").innerHTML= oranges;
+
+
+document.getElementById("sub-topic-modulus").innerHTML="4. MODULUS ASSIGNMENT: The modulus assignment operator assigns a remainder to a variable.";
+let money = 1000000;
+money %= 50;
+document.getElementById("modulus-illustration").innerHTML="<code> let money = 1000000; </code><br> <code> money %= 50;</code>";
+document.getElementById("modulus-ans").innerHTML= money;
+
+
+document.getElementById("sub-topic-expo").innerHTML="5. EXPONENTIAL ASSIGNMENT: The exponentiation operator returns the result of raising the first variable to the power of the second variable.";
+let wallet = 4;
+wallet **= 2;
+document.getElementById("expo-illustration").innerHTML="<code> let squareroots = 4; </code><br> <code> squareroots **= 2;</code>";
+document.getElementById("expo-ans").innerHTML= squareroots;
+
+
+
+
+ 
+ 
+
